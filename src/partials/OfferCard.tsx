01a8896118842fdaf1cea19647c87a93f389ef50
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-11 15:39:28 +0530

    fix: offercard css hover effect

--- src/partials/OfferCard.tsx
+++ src/partials/OfferCard.tsx
@@ -39,11 +39,11 @@ const OfferCard = () => {
 
             </div>
 
-            <div className="price w-full">
-            <div className="bg-[#ecba00] w-full text-center py-4">
-                <p className="text-white text-sm">$4,500</p>
-            </div>
-            </div>
+
+                <div className=" price bg-[#ecba00] w-full text-center py-4  hover:bg-[#222]">
+                    <p className="text-white text-sm">$4,500</p>
+                </div>
+            
         </div>
         
     </div>
