c84de67575509c1e11895dae5f483c52fdbc26e0
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-11 23:57:43 +0530

    feat: add About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,68 @@
+import { HiBuildingOffice2 } from "react-icons/hi2";
+import { FaTrowelBricks } from "react-icons/fa6";
+import { BsWindowDesktop } from "react-icons/bs";
+import aboutImg from "../assets/aboutImg.png"
+import aboutImg2 from "../assets/aboutImg2.png"
 import Title from "../partials/Title"
+
+const items = [
+    {
+        title:"Affordable Auto Prices",
+        description:"AutoDrive is nisi aliquip cibsequat duis velit esse",
+        icon: <HiBuildingOffice2 size={30} color={"#222"} />
+    },
+    {
+        title:"10 Years in Business",
+        description:"AutoDrive is nisi aliquip cibsequat duis velit esse",
+        icon: <FaTrowelBricks size={30} color={"#222"} />
+    },
+    {
+        title:"Affordable Auto Prices",
+        description:"AutoDrive is nisi aliquip cibsequat duis velit esse",
+        icon: <BsWindowDesktop size={30} color={"#222"} />
+    }
+]
 
 const About = () => {
   return (
-    <section className="about py-20">
-    <Title title1="About" title2="Autodrive" description="SOMETHING ABOUT COMPANY" bg="light"/>
+    <section className="about py-20 max-w-[1200px] mx-auto">
+        <Title title1="About" title2="Autodrive" description="SOMETHING ABOUT COMPANY" bg="light"/>
+        
+        <div className="flex flex-col lg:flex-row mt-24">
+            <div className="text-[#222] w-11/12 lg:w-6/12 h-full my-4 mx-auto">
+                <h3 className="text-md font-bold">We are a Trusted Name in Auto Industry</h3>
+                <h1 className="text-xl font-bold my-2">Visited by Million of Car Buyers Every Month!</h1>
+                <p className="text-sm text-[#666] mt-4 mb-12">Autodealer is nisi aliquip consequat duis velit esse cillum dolore
+                     fugiat nulla pariatur excepteur sint occaecat. Lorem ipsum dolor 
+                     sit amet consectetur adipisicing elit sed eiusmod tempor incididuntu
+                      labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation
+                       ullamco laboris nisi aliquip. Duis aute irure dolor in reprehenderit in
+                        voluptate velit ese cillum dolore fugiat nulla pariatur excepteur sint
+                         occaecat cupidatat non proident.
+                </p>
+
+                {
+                    items.map((item, index) => (
+                        <div key={index}>
+                            <div className="flex items-center my-4">
+                                <div className=" p-2 rounded-full mr-4">
+                                    {item.icon}
+                                </div>
+                                <div>
+                                    <h3 className="text-md font-semibold">{item.title}</h3>
+                                    <p className="text-sm text-[#666]">{item.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className="w-11/12  lg:w-6/12 px-4 relative">
+                <img src={aboutImg} alt="about" className=" absolute  top-8 w-full "/>
+                <img src={aboutImg2} alt="about2" className="absolute left-8  w-full "/>
+            </div>
+        </div>
     </section>
   )
 }
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -38,9 +38,9 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col items-center mt-4">
-          <div className=" h-[2px] bg-[#666] w-[50px]" ></div>
-          <div className=" h-[3px] bg-[#666] w-[50px] my-1 translate-x-1/3" ></div>
-          <div className=" h-[2px] bg-[#666] w-[50px] translate-x-2/3" ></div>
+                    <div className=" h-[3px] bg-[#666] w-[50px]" ></div>
+                    <div className=" h-[2px] bg-[#666] w-[50px] my-1 translate-x-1/3" ></div>
+                    <div className=" h-[3px] bg-[#666] w-[50px] translate-x-2/3" ></div>
         </div>
 
         <div className="wrapper mt-20">
