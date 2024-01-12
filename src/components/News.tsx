0b19e0fe3bae69dd304b44c40b9e4411c4bc11bb
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-12 17:57:30 +0530

    fix: center blog elements

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -13,10 +13,12 @@ const News = () => {
         bg="light"
         />
 
-        <div className="max-w-[1200px] mx-auto mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        <div className="max-w-[1200px] mx-auto mt-16 flex justify-center">
+          <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">    
             <Blog/>
             <Blog/>
             <Blog/>
+          </div>
         </div>
     </section>
   )
