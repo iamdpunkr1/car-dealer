6e30ab47b68d3406da7a7f16c34336ca2899ad95
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-17 12:56:25 +0530

    style: member hover animation

--- src/partials/Member.tsx
+++ src/partials/Member.tsx
@@ -1,12 +1,27 @@
 import memberImg from "../assets/memberImg.jpg"
-
+import SocialIcons from "./SocialIcons"
 
 const Member = () => {
   return (
-    <div className="h-[383px]  bg-transparent cursor-move m-box " style={{width:"95%"}}>
-        <img src={memberImg} alt="memberImg" className="max-w-[8/12] mx-auto" loading="lazy"/>
+    <div className="h-[383px] cursor-move  " style={{width:"95%"}}>
+      <div className="m-box">
+        <img src={memberImg} alt="memberImg" className="max-w-full mx-auto" loading="lazy"/>
         
-        <div className="m-content">
+        <div className="m-overlay">
+          <div className="m-content text-center py-4 flex flex-col justify-end">
+            <h1 className="text-lg font-semibold">
+                Diogo Johnson
+            </h1>
+            <p className="uppercase text-slate-50 ">Partner</p>
+            <div className="flex justify-center gap-2 mt-4">
+            <SocialIcons color="#fff" size={18}/>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+
+        <div className="m-content2">
           <div className="text-center py-4 ">
             <h1 className="text-lg font-semibold">
                 Diogo Johnson
