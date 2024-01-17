bb165ad732e013c8790fe0dfcaaa3b9739db1fa1
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-17 15:47:01 +0530

    fix: bottom padding of skewed divs

--- src/partials/SkewedCars.tsx
+++ src/partials/SkewedCars.tsx
@@ -14,7 +14,7 @@ const SkewedCars = ({ img, title, offers, right }: SkewedCarsProps) => {
   return (
     <a href="" className="w-[90%] md:lg:w-full hid mx-auto" onClick={handleClick}>
       <div
-        className={`bg-white py-4 test flex flex-col  justify-center gap-8 items-center ${right ? "md:lg:flex-row-reverse" : " md:lg:flex-row"} px-4`}
+        className={`bg-white pt-4 pb-4 lg:pb-2 test flex flex-col  justify-center gap-8 items-center ${right ? "md:lg:flex-row-reverse" : " md:lg:flex-row"} px-4`}
       >
         <div className="bottom-4">
           <img src={img} alt={title} className="w-[90%] md:w-44 lg:40 mx-auto" loading="lazy" />
